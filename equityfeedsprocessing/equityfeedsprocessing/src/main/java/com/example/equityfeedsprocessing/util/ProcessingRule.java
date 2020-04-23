3f22aba56c61c6492ada68daa1f757b7cdd5161f
sid2279 <64156572+sid2279@users.noreply.github.com>
2020-04-23 22:02:52 +0530

    Update ProcessingRule.java

--- equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/util/ProcessingRule.java
+++ equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/util/ProcessingRule.java
@@ -4,6 +4,7 @@ import com.example.equityfeedsprocessing.model.EquityFeeds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Component
 public class ProcessingRule {
 
     private static final Logger logger = LoggerFactory.getLogger(ProcessingRule.class);
