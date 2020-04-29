e0e67ffec97081342483f14916caf99429801155
sid2279 <64156572+sid2279@users.noreply.github.com>
2020-04-29 19:28:30 +0530

    Update CapitalIQSubscriber.java
    Committed the changes to the file.

--- equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/listener/CapitalIQSubscriber.java
+++ equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/listener/CapitalIQSubscriber.java
@@ -1,52 +1,39 @@
 package com.example.equityfeedsprocessing.listener;
 
-import com.example.equityfeedsprocessing.impl.EquityFeedsRedisRepositoryImpl;
+
 import com.example.equityfeedsprocessing.model.EquityFeeds;
-import com.example.equityfeedsprocessing.repository.EquityFeedsRedisRepository;
-import com.example.equityfeedsprocessing.repository.EquityFeedsRepository;
+import com.example.equityfeedsprocessing.util.POJOValidator;
 import com.example.equityfeedsprocessing.util.ProcessingLogic;
-import com.example.equityfeedsprocessing.util.SavingEquityData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.ObjectMessage;
+import javax.validation.ConstraintViolation;
+import java.util.Set;
 
 @Component
 @EnableJms
 public class CapitalIQSubscriber {
 
     @Autowired
-    private EquityFeedsRepository equityFeedsRepository;
-
-    @Autowired
-    private EquityFeedsRedisRepository equityFeedsRedisRepository;
-
-    @Autowired
-    private EquityFeedsRedisRepositoryImpl equityFeedsRedisRepositoryImpl;
-
-    @Autowired
-    private JmsTemplate jmsTemplate;
-
-    @Autowired
-    private SavingEquityData savingEquityData;
-
-    @Autowired
     private ProcessingLogic processingLogic;
+
+    @Autowired
+    private POJOValidator pojoValidator;
 
     @Value("${capitalIQ.outboundTopicName}")
     private String capitalIQOutboundTopicName;
 
     private static final Logger logger = LoggerFactory.getLogger(CapitalIQSubscriber.class);
 
-//    @JmsListener(destination = "${capitalIQ.inboundTopicName}", containerFactory = "jmsListenerContainerFactory")
+    @JmsListener(destination = "${capitalIQ.inboundTopicName}", containerFactory = "jmsListenerContainerFactory")
     public void capitalIQProcessor(final Message objectMessage) {
 
         logger.info("Entered the CapitalIQ Processor method.");
@@ -73,11 +60,18 @@ public class CapitalIQSubscriber {
 
             EquityFeeds equityFeeds = (EquityFeeds) object;
 
-            logger.info("POJO returned is: {}", equityFeeds);
+            logger.info("Calling the validate method of the POJO validator class to Validate the POJO object.");
 
-            logger.info("Calling the Processing Logic class to process the POJO as per Intra-day / Normal transaction.");
+            Set<ConstraintViolation<EquityFeeds>> constraintViolations = pojoValidator.validate(equityFeeds);
 
-            processingLogic.processDataLogic(capitalIQOutboundTopicName,equityFeeds);
+            if (constraintViolations.size() > 0) {
+                for (ConstraintViolation<EquityFeeds> violation : constraintViolations) {
+                    logger.error(violation.getMessage());
+                }
+            } else {
+                logger.info("Valid Object. Sending the object for processing.");
+                processingLogic.processDataLogic(capitalIQOutboundTopicName,equityFeeds);
+            }
 
         }
 
