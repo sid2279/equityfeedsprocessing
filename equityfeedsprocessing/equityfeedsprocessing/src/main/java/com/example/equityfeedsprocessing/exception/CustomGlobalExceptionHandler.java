4865b039d096ca981775d8e8fbb0c612683f89a9
sid2279 <64156572+sid2279@users.noreply.github.com>
2020-04-30 10:45:22 +0530

    Update CustomGlobalExceptionHandler.java
    Committing the changes

--- equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/exception/CustomGlobalExceptionHandler.java
+++ equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/exception/CustomGlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @ControllerAdvice
 @RestController
@@ -18,28 +18,28 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
     @ExceptionHandler(ExternalTransactionIdNotFoundException.class)
     @ResponseBody
     public final ResponseEntity<ExceptionResponse> handleExternalTransactionIdNotFound(ExternalTransactionIdNotFoundException externalTransactionIdNotFoundException, WebRequest request) throws Exception {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), externalTransactionIdNotFoundException.getMessage(), request.getDescription(false), HttpStatus.NOT_ACCEPTABLE.getReasonPhrase(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value());
+        ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDate.now(), externalTransactionIdNotFoundException.getMessage(), request.getDescription(false), HttpStatus.NOT_ACCEPTABLE.getReasonPhrase(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value());
         return new ResponseEntity<ExceptionResponse> (exceptionResponse, HttpStatus.NOT_ACCEPTABLE);
     }
 
     @ExceptionHandler(ClientIdNotFoundException.class)
     @ResponseBody
     public final ResponseEntity<ExceptionResponse> handleClientIdNotFound(ClientIdNotFoundException clientIdNotFoundException, WebRequest request) throws Exception {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), clientIdNotFoundException.getMessage(), request.getDescription(false), HttpStatus.NOT_ACCEPTABLE.getReasonPhrase(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value());
+        ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDate.now(), clientIdNotFoundException.getMessage(), request.getDescription(false), HttpStatus.NOT_ACCEPTABLE.getReasonPhrase(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value());
         return new ResponseEntity<ExceptionResponse> (exceptionResponse, HttpStatus.NOT_ACCEPTABLE);
     }
 
     @ExceptionHandler(SecurityIdNotFoundException.class)
     @ResponseBody
     public final ResponseEntity<ExceptionResponse> handleSecurityIdNotFound(SecurityIdNotFoundException securityIdNotFoundException, WebRequest request) throws Exception {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), securityIdNotFoundException.getMessage(), request.getDescription(false), HttpStatus.NOT_ACCEPTABLE.getReasonPhrase(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value());
+        ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDate.now(), securityIdNotFoundException.getMessage(), request.getDescription(false), HttpStatus.NOT_ACCEPTABLE.getReasonPhrase(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value());
         return new ResponseEntity<ExceptionResponse> (exceptionResponse, HttpStatus.NOT_ACCEPTABLE);
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false), HttpStatus.NOT_ACCEPTABLE.getReasonPhrase(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value());
+        ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDate.now(), ex.getMessage(), request.getDescription(false), HttpStatus.NOT_ACCEPTABLE.getReasonPhrase(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value());
         return new ResponseEntity<ExceptionResponse> (exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
