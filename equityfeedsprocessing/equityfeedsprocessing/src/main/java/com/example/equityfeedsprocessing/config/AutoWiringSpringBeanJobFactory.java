8a35dc1d6f21c74dbd0e8d9bd5b5f7b2e539bae0
sid2279 <64156572+sid2279@users.noreply.github.com>
2020-04-30 10:40:58 +0530

    Update AutoWiringSpringBeanJobFactory.java
    Committing the changes

--- equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/config/AutoWiringSpringBeanJobFactory.java
+++ equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/config/AutoWiringSpringBeanJobFactory.java
@@ -7,9 +7,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
-public final class AutoWiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+public class AutoWiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
 
-    private transient AutowireCapableBeanFactory beanFactory;
+    private AutowireCapableBeanFactory beanFactory;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
