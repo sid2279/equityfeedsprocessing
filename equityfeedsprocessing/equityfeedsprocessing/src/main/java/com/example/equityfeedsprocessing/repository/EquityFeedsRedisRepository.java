26474c9b982126d0d45d8b76b3e27230f5f94cf8
sid2279 <64156572+sid2279@users.noreply.github.com>
2020-04-30 10:52:33 +0530

    Update EquityFeedsRedisRepository.java
    Committing the changes.

--- equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/repository/EquityFeedsRedisRepository.java
+++ equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/repository/EquityFeedsRedisRepository.java
@@ -3,7 +3,6 @@ package com.example.equityfeedsprocessing.repository;
 import com.example.equityfeedsprocessing.model.EquityFeeds;
 
 import java.util.List;
-import java.util.Map;
 
 public interface EquityFeedsRedisRepository {
 
@@ -11,12 +10,9 @@ public interface EquityFeedsRedisRepository {
     EquityFeeds saveExternalTransactionId(EquityFeeds equityFeeds);
     EquityFeeds saveClientId(EquityFeeds equityFeeds);
     EquityFeeds saveSecurityId(EquityFeeds equityFeeds);
-    Map<String, EquityFeeds> findAll();
     EquityFeeds findById(String id);
     EquityFeeds findByExternalTransactionId(String externalTransactionId);
     List<EquityFeeds> findByClientId(String clientId);
     List<EquityFeeds> findBySecurityId(String securityId);
-    void update(EquityFeeds equityFeeds);
-    void delete(String id);
 
 }
