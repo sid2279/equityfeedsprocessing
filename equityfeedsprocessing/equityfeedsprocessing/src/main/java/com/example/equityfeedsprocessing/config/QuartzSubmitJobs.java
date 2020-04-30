ec78523299bad6f438fe538d9398b1304cc9ad69
sid2279 <64156572+sid2279@users.noreply.github.com>
2020-04-30 10:43:32 +0530

    Update QuartzSubmitJobs.java
    Committing the changes.

--- equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/config/QuartzSubmitJobs.java
+++ equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/config/QuartzSubmitJobs.java
@@ -9,15 +9,10 @@ import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 @Configuration
 public class QuartzSubmitJobs {
 	
-//	private static final String CRON_EVERY_FIVE_MINUTES = "0 0/5 * ? * * *";
-
-    // Fires At 11:20 AM every Monday, Tuesday, Wednesday, Thursday and Friday.
-    // seconds minutes hours day_of_month month day_of_week Year
-    private static final String CRON_EVERY_WEEKDAY = "0 52 11 ? * MON-FRI";
+    private static final String CRON_EVERY_WEEKDAY = "0 28 11 ? * MON-FRI";
 
     @Bean(name = "memberStatsTrigger")
     public CronTriggerFactoryBean triggerMemberClassStats(@Qualifier("memberStats") JobDetail jobDetail) {
-//    	return QrtzScheduler.createCronTrigger(jobDetail, CRON_EVERY_FIVE_MINUTES, "Sample Job Trigger");
         return QrtzScheduler.createCronTrigger(jobDetail, CRON_EVERY_WEEKDAY, "Sample Job Trigger");
     }
 
