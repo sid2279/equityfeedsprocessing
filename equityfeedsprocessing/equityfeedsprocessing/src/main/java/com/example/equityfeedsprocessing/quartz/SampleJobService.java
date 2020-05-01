4d3fcd973248d040e65d06e398de328a7b749255
sid2279 <64156572+sid2279@users.noreply.github.com>
2020-05-01 12:39:30 +0530

    Update SampleJobService.java
    Committed the changes made.

--- equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/quartz/SampleJobService.java
+++ equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/quartz/SampleJobService.java
@@ -37,9 +37,9 @@ public class SampleJobService {
 
     private static final Logger logger = LoggerFactory.getLogger(SampleJobService.class);
 
-    public static final String CSV_FILE = "src/main/resources/nasdaqdatalarge.csv";
+    public static final String CSV_FILE = "src/main/resources/nasdaqdata_invalid_1.csv";
 
-    public void executeSampleJob() {
+    public void executeSampleJob() throws IOException {
 
         logger.info(" START | In executeSampleJob method of SampleJobService class.");
 
@@ -53,27 +53,27 @@ public class SampleJobService {
             ftpClient.enterLocalPassiveMode();
             ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
 
-            String remoteFile1 = "/equityFeedsProcessing/nasdaqdatalarge.csv";
+            String remoteFile1 = "/equityFeedsProcessing/nasdaqdata_invalid_1.csv";
             File downloadFile1 = new File(CSV_FILE);
-            OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
-            boolean success = ftpClient.retrieveFile(remoteFile1, outputStream1);
-            outputStream1.close();
+            try(OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1))) {
+                boolean success = ftpClient.retrieveFile(remoteFile1, outputStream1);
+                outputStream1.close();
 
-            if (success) {
-                logger.info("File downloaded successfully from remote FTP server.");
+                if (success) {
+                    logger.info("File downloaded successfully from remote FTP server.");
 
-                logger.info("START | Calling the parseCSVFile method of the parseCSVFile class.");
+                    logger.info("START | Calling the parseCSVFile method of the parseCSVFile class.");
 
-                parseCSVFile.parse();
+                    parseCSVFile.parse();
 
-                logger.info("END | Calling the parseCSVFile method of the parseCSVFile class.");
+                    logger.info("END | Calling the parseCSVFile method of the parseCSVFile class.");
 
-            } else {
-                logger.error("File could not be downloaded from remote FTP Server.");
+                } else {
+                    logger.error("File could not be downloaded from remote FTP Server.");
+                }
+
+                logger.info(" END | In executeSampleJob method of SampleJobService class.");
             }
-
-            logger.info(" END | In executeSampleJob method of SampleJobService class.");
-
         } catch (IOException ex) {
             logger.error("IO Exception: {}", ex);
         } finally {
