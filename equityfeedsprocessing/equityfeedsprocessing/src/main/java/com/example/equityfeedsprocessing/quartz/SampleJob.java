b3fe5d5012e69caed2979a64bb31dc48aeb74ad4
sid2279 <64156572+sid2279@users.noreply.github.com>
2020-05-01 12:38:49 +0530

    Update SampleJob.java
    Committed the changes.

--- equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/quartz/SampleJob.java
+++ equityfeedsprocessing/equityfeedsprocessing/src/main/java/com/example/equityfeedsprocessing/quartz/SampleJob.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class SampleJob implements Job {
 	
@@ -25,7 +27,11 @@ public class SampleJob implements Job {
 
 		logger.info(" START | Sample Job executed.");
 
-		sampleJobService.executeSampleJob();
+		try {
+			sampleJobService.executeSampleJob();
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
 
 		logger.info(" END | Sample Job executed.");
 	}	
